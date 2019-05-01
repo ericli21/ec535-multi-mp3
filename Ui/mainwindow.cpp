3b717d1ff03231c1de94d78942b41bf4ab59f94b
Li <ericli21@vlsi48.bu.edu>
2019-05-01 18:33:21 -0400

    Added song on window

--- Ui/mainwindow.cpp
+++ Ui/mainwindow.cpp
@@ -183,7 +183,7 @@ void MainWindow::playSong(int state) {
 			current_interval = (current_song_node->info->length_seconds * 1000) + 2000;
 			std::cout << current_interval << "= current interval\n";
 
-			//emit changeSongName(current_song_node->info->song_title);
+			emit changeSongName(current_song_node->info->song_title);
 
 			songTimer -> setInterval(current_interval);
 			//songTimer -> start(current_interval);
@@ -202,7 +202,7 @@ void MainWindow::playSong(int state) {
 			std::cout << "play button activate: " << command << "\n";
 			system(command2);
 
-			//emit changeSongName(current_song_node->info->song_title);
+			emit changeSongName(current_song_node->info->song_title);
 
 			current_interval = (current_song_node->info->length_seconds * 1000) + 2000;
 			std::cout << current_interval << "= current interval\n";
@@ -259,7 +259,7 @@ void MainWindow::forwardSong() {
 
 		system(command2);
 		
-		//emit changeSongName(current_song_node->info->song_title);
+		emit changeSongName(current_song_node->info->song_title);
 
 		//songTimer -> setInterval(current_song_node->info->length_seconds * 1000);
 		songTimer -> start(current_interval);
@@ -290,7 +290,7 @@ void MainWindow::backSong() {
 
 		system(command2);
 		
-		//emit changeSongName(current_song_node->info->song_title);
+		emit changeSongName(current_song_node->info->song_title);
 
 		//songTimer -> setInterval(current_song_node->info->length_seconds * 1000);
 		songTimer -> start(current_interval);
@@ -315,7 +315,7 @@ void MainWindow::transition() {
 
 		system(command2);
 				
-		//emit changeSongName(current_song_node->info->song_title);
+		emit changeSongName(current_song_node->info->song_title);
 		
 		//songTimer -> setInterval(current_song_node->info->length_seconds * 1000);
 		songTimer -> start(current_interval);	
--- Ui/moc_lockmenu.cpp
+++ Ui/moc_lockmenu.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'lockmenu.h'
 **
-** Created: Wed May 1 18:28:49 2019
+** Created: Wed May 1 18:33:00 2019
 **      by: The Qt Meta Object Compiler version 61 (Qt 4.5.1)
 **
 ** WARNING! All changes made in this file will be lost!
