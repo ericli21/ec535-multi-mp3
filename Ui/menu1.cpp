b0134faba7c81cd42c9f9c14806233678925bbb1
Ekchian <cekchian@vlsi50.bu.edu>
2019-05-01 21:22:40 -0400

    Cleaned up menu1.cpp. Removed unneeded lines and added comments.

--- Ui/menu1.cpp
+++ Ui/menu1.cpp
@@ -16,58 +16,54 @@
 
 Menu1::Menu1(MainWindow *parent) : QWidget(parent)
 {	
-	std::cout << "Test11\n";
+	//Initialize buttons, listWidget to display songs, layouts
 	addButton = new QPushButton("Add a song", this);
 	backButton = new QPushButton("Menu", this);
-
 	QListWidget *listWidget = new QListWidget(this);
-	
-	std::cout << "Test12\n";
 	QVBoxLayout *layout = new QVBoxLayout();
 	QHBoxLayout *layout2 = new QHBoxLayout();
+
+	//Search directory for songs
+	//If there is an mp3, display it on the listWidget
 	QDir fdir("/media/card/songs/");
 	QStringList allSongs = fdir.entryList();
 	for (int i = 0; i < allSongs.size(); ++i) {
-		std::cout << qPrintable(allSongs.at(i)) << "\n";
 		if(strstr(qPrintable(allSongs.at(i)), ".mp3") != NULL) {
-			//if (qPrintable(allSongs.at(i)) != ".." || qPrintable(allSongs.at(i)) != ".") {
 			QListWidgetItem *newItem = new QListWidgetItem;
 			newItem->setText(qPrintable(allSongs.at(i)));
 			listWidget->insertItem(i, newItem);
 		}
 	}
-	std::cout << "Test13\n";
-	QLabel *message = new QLabel("Play a song", this);
+
+	//Set up layout
+	QLabel *message = new QLabel("Choose a song", this);
 	layout -> addWidget(message);
 	layout -> addWidget(listWidget);
-	
-
-	std::cout << "Test14\n";
 	layout2 -> addWidget(addButton);
 	layout2 -> addWidget(backButton);
 	layout -> addLayout(layout2);
 	setLayout(layout);
 
-	std::cout << "Test15\n";
+	//Update currently selected song
 	connect(listWidget, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(slotTest(QListWidgetItem*)));
 
-
+	//Map signals from add and back buttons to correct 
 	connect(addButton, SIGNAL(released()), this, SLOT(confirmSong()));
 	connect(this, SIGNAL(addSong(std::string)), parent, SLOT(pushSong(std::string)));
 	connect(backButton, SIGNAL(released()), parent, SLOT(goToMenu()));
 
-	std::cout << "Test17\n";
+	//Update timeout once a button or scroll through the list has been activated
 	connect(listWidget, SIGNAL(itemClicked(QListWidgetItem*)), parent, SLOT(updateTimeout()));
 	connect(addButton, SIGNAL (released()), parent, SLOT (updateTimeout()));
 	connect(backButton, SIGNAL (released()), parent, SLOT (updateTimeout()));
-
 }
 
-
+//Set selected variable as the selected song from listWidget
 void Menu1::slotTest(QListWidgetItem* song) {
 	selected = qPrintable(song->text());
 }
 
+//Confirm that the song is just a empty string
 void Menu1::confirmSong() {
 	if (!selected.empty()) {
 		std::cout << selected << "added to queue.\n";
