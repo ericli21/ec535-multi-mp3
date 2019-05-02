a411e49e5ab82c8c805fa2c71ba093dc83e47f37
Ekchian <cekchian@vlsi50.bu.edu>
2019-05-01 21:17:57 -0400

    Updated mainmenu.cpp and mainwindow.cpp. Removed some print lines and added some comments.

--- Ui/mainmenu.cpp
+++ Ui/mainmenu.cpp
@@ -16,8 +16,9 @@
 
 #include "mainmenu.h"
 
+//Main Menu shows up after first logging into the device
+
 MainMenu::MainMenu(MainWindow *parent) : QWidget(parent) {
-	std::cout << "Test7\n";
 	state = 0;
 	backButton = new QPushButton("Log out", this);
 	skipButton = new QPushButton("Forward", this);
@@ -28,7 +29,6 @@ MainMenu::MainMenu(MainWindow *parent) : QWidget(parent) {
 
 	//layout = new QHBoxLayout();
 	layout = new QGridLayout();
-	std::cout << "Test8\n";
 	current_song = new QLabel("No song playing currently", this);
 	current_artist = new QLabel(" ", this);
 	current_album = new QLabel(" ", this);
@@ -39,7 +39,6 @@ MainMenu::MainMenu(MainWindow *parent) : QWidget(parent) {
 	layout -> addWidget(my_Button, 0, 1, 1, 1);
 	layout -> addWidget(skipButton, 0, 2, 1, 1);
 	layout -> addWidget(backButton, 5, 0, 1, 3);
-	std::cout << "Test9\n";
 	setLayout(layout);
 	connect(backButton, SIGNAL(released()), parent, SLOT(goToLock()));
 	connect(my_Button, SIGNAL (released()), parent, SLOT(checkNodes()));
@@ -50,7 +49,6 @@ MainMenu::MainMenu(MainWindow *parent) : QWidget(parent) {
 	connect(this, SIGNAL(backSig()), parent, SLOT(backSong()));
 	connect(this, SIGNAL(forwardSig()), parent, SLOT(forwardSong()));
 
-	std::cout << "Test10\n";
 	connect(signalMapper, SIGNAL (mapped(int)), parent, SLOT (updateTimeout()));
 	connect(backButton, SIGNAL (released()), parent, SLOT (updateTimeout()));
 	connect(my_Button, SIGNAL (released()), parent, SLOT (updateTimeout()));
@@ -63,12 +61,9 @@ MainMenu::MainMenu(MainWindow *parent) : QWidget(parent) {
 
 	connect(this, SIGNAL (play_to_pause(int)), parent, SLOT(pauseSong(int)));
 	connect(this, SIGNAL (pause_to_play(int)), parent, SLOT(playSong(int)));
-	//connect(skipButton, SIGNAL (released()), parent SLOT (checkPriority()));
-	//connect(previousButton, SIGNAL (released()), parent, SLOT (checkPriority()));
 
 	connect(parent, SIGNAL(changeSongName(std::string,std::string,std::string)), this, SLOT(updateName(std::string,std::string,std::string)));
 
-	//connect(parent, SIGNAL(noSong()), this, SLOT(correctState()));
 
 	nButtons = 0;
 }
@@ -85,50 +80,29 @@ void MainMenu::handleButton()
 {
 	// change the text
 	if (state) {
-		
-		//system("ash speaker_interface.sh pause &");
 		emit play_to_pause(state);
 
 		state = 0;
 		my_Button->setText("Play");
-		//QCoreApplication::processEvents();
 	}
 	else {
-		//system("ash speaker_interface.sh play &");
 		emit pause_to_play(state);
 	
 		state = 1;
 		my_Button->setText("Pause");
-		//QCoreApplication::processEvents();
 	}
-	// resize button
-	//my_button->resize(100,100);
 }
 
 void MainMenu::handlePrevious() {
 	state = 1;
 	my_Button->setText("Pause");
 	emit backSig();
-	//std::string nextSong = "blank";
-	//std::string line = "ash speaker_interface.sh next ";
-	//line += nextSong;
-	//line += " &";
-	//line <<  "ash speaker_interface.sh next " << nextSong << " &"; 
-			
-	//system(line.c_str());
 }
 
 void MainMenu::handleSkip() {
 	state = 1;
 	my_Button->setText("Pause");
 	emit forwardSig();
-	//std::string previousSong = "blank1";
-	//std::string line = "ash speaker_interface.sh next ";
-	//line += previousSong;
-	//line += " &";
-	//line <<  "ash speaker_interface.sh next " << previousSong << " &"; 
-			
-	//system(line.c_str());
 }
 
 void MainMenu::updateName(std::string songName, std::string artistName, std::string albumName) {
@@ -136,9 +110,3 @@ void MainMenu::updateName(std::string songName, std::string artistName, std::str
 	current_artist->setText(QString::fromStdString(artistName));
 	current_album->setText(QString::fromStdString(albumName));
 }
-
-//void MainMenu::correctState() {
-//	state = 0;
-//	my_Button->setText("Play");
-//}
-
